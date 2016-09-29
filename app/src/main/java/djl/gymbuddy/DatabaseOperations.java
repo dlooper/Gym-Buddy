e53f24154fc5ea5782fe33e23090112461b1e131
dlooper <looper@seawolf.sonoma.edu>
2016-09-29 14:38:53 -0700

    * Need to be able to find latest database entry for a workout -- will use this as a comparison tool for how many sets / reps / weight to do.           - Determined how to find current date, and input it into the database           - Determined how to compare dates to find the latest database entry
    * Started working on retrieving data from the database
              - Able to retrieve data -- now I need to tailor it to specifically retrieve the data I want and find the relevant information to display.
    
    * Searched for a way to display all of my database table entries in the log files
              -Created a class for this, which now functions properly.

--- app/src/main/java/djl/gymbuddy/DatabaseOperations.java
+++ app/src/main/java/djl/gymbuddy/DatabaseOperations.java
@@ -2,6 +2,7 @@ package djl.gymbuddy;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -42,5 +43,35 @@ public class DatabaseOperations extends SQLiteOpenHelper {
 
         long k = SQ.insert(WorkoutTable.WorkoutInfo.TABLE_NAME, null, cv);
         Log.d("Database operations", "One row inserted");
+    }
+
+    public Cursor getInformation(DatabaseOperations dop) {
+        SQLiteDatabase SQ = dop.getReadableDatabase();
+        String test = getTableAsString(SQ);
+        Log.v("test", test);
+
+        String[] columns = {WorkoutTable.WorkoutInfo.WORKOUT, WorkoutTable.WorkoutInfo.DATE};
+        Cursor CR = SQ.query(WorkoutTable.WorkoutInfo.TABLE_NAME, columns, null, null, null, null, null);
+        return CR;
+    }
+
+    public String getTableAsString(SQLiteDatabase db) {
+        String tableName = WorkoutTable.WorkoutInfo.TABLE_NAME;
+        Log.d("DbHelper", "getTableAsString called");
+        String tableString = String.format("Table %s:\n", tableName);
+        Cursor allRows  = db.rawQuery("SELECT * FROM " + tableName, null);
+        if (allRows.moveToFirst() ){
+            String[] columnNames = allRows.getColumnNames();
+            do {
+                for (String name: columnNames) {
+                    tableString += String.format("%s: %s\n", name,
+                            allRows.getString(allRows.getColumnIndex(name)));
+                }
+                tableString += "\n";
+
+            } while (allRows.moveToNext());
+        }
+
+        return tableString;
     }
 }
--- app/src/main/java/djl/gymbuddy/MainActivity.java
+++ app/src/main/java/djl/gymbuddy/MainActivity.java
@@ -2,12 +2,14 @@ package djl.gymbuddy;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,21 +17,42 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
 public class MainActivity extends AppCompatActivity {
     String WORKOUT, DATE;
-    EditText REPS, SETS, WEIGHT;
-    String reps_str, sets_str, weight_str;
+    EditText REPS, SETS, WEIGHT, WORKOUT1, DATE1;
+    String reps_str, sets_str, weight_str, workout1, date1;
     Integer reps, sets, weight;
     Button COMPLETE;
     Context ctx = this;
-
+    //********************************************************
+    Calendar c = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+    String formattedDate = sdf.format(c.getTime());
+    String beforeDate = "09-28-2016";
+    //********************************************************
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DatabaseOperations DOP = new DatabaseOperations(ctx);
+        Cursor CR = DOP.getInformation(DOP);
+        CR.moveToFirst();
+        boolean loginStatus = false;
+        String NAME = "";
+        do {
+
+        }while(CR.moveToNext());
+
         WORKOUT = "UpperChest";
-        DATE = "9/28/2016";
+        DATE = "9/29/2016";
         REPS = (EditText) findViewById(R.id.ex1_reps);
         SETS = (EditText) findViewById(R.id.ex1_sets);
         WEIGHT = (EditText) findViewById(R.id.ex1_wght);
@@ -46,7 +69,14 @@ public class MainActivity extends AppCompatActivity {
 
                 DatabaseOperations DB = new DatabaseOperations(ctx);
                 DB.insertValues(DB, WORKOUT, DATE, reps, weight, sets);
-                Toast.makeText(getBaseContext(), "Successfully Inserted Data", Toast.LENGTH_LONG).show();
+                //Toast.makeText(getBaseContext(), "Successfully Inserted Data", Toast.LENGTH_LONG).show();
+                /*
+                if (beforeDate.compareTo(formattedDate) < 0) {
+                    Log.d("formattedDate is", "latest");
+                }
+                else {
+                    Log.d("beforeDate is", "latest");
+                }*/
             }
         });
     }
